1cf6647517214c4f1d6a5b5f633e3244deaa33b5
Maiki <maikpeters1511@gmail.com>
2023-06-27 14:53:51 +0200

    Beispiele in app.ts

--- app.ts
+++ app.ts
@@ -7,4 +7,9 @@ const todoList = new TodoList<Metadata>()
 todoList.note("Code aufräumen", {date: Date(), assignee: "Maik"})
 todoList.note("SQL Lernen", {date: Date()})
 
+const improveErrorHandlingId = todoList.note("Felerbehandlung verbessern",
+    {date: Date(), assignee: "Maik"});
+
+todoList.edit(improveErrorHandlingId, "Fehlerbehandlung verbessern")
+todoList.markOff(improveErrorHandlingId);
 console.log(todoList.getAllTodos())
